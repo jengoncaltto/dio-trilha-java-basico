1c63c83872cfdde6140831a58b6a6c56d45a4e08
Jenifer <jenifergonbrito@gmail.com>
2024-06-05 22:16:57 -0300

    adicionando a primeira versão do código todo

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,3 +1,18 @@
 public class ContaTerminal {
+    public int numero;
+    public String agencia;
+    public String nomeCliente;
+    public float saldo;
 
+    public void nomeCliente(String nome) {
+    }
+
+    public void agencia(String agencia) {
+    }
+
+    public void numero(int conta) {
+    }
+
+    public void saldo(float saldo) {
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception{
+        Scanner scan = new Scanner(System.in);
+        ContaTerminal contaTerminal = new ContaTerminal();
+
+        System.out.println("---CONTA---");
+        System.out.println("Por favor insira o seu nome: ");
+        String nome = scan.nextLine();
+        System.out.println("Por favor insira a sua agência: ");
+        String agencia = scan.nextLine();
+        System.out.println("Por favor insira o numero de sua agencia: ");
+        int conta = scan.nextInt();
+        System.out.println("Por favor insira o numero do seu saldo: ");
+        float saldo = scan.nextFloat();
+
+        contaTerminal.nomeCliente(nome);
+        contaTerminal.agencia(agencia);
+        contaTerminal.numero(conta);
+        contaTerminal.saldo(saldo);
+
+        System.out.println("Olá " + nome.concat(", obrigado por criar uma conta em nosso banco, ") +
+                "sua agência é ".concat(agencia) + ", conta " + conta + " e seu saldo " + saldo +
+                " já está disponível para saque");
     }
 }
